dd18e822ee123f4711abd5bf4bda4145293e7e03
MikkoKarvonen <mkarvonen34@gmail.com>
2019-02-27 19:23:22 +0200

    Add scroll to top button

--- js/script.js
+++ js/script.js
@@ -32,3 +32,13 @@ $('#carousel').on('slid.bs.carousel', function () {
     addressEnd = 'hannajurvelin.com';
     $('#contactMail').attr('href','mailto:'+addressStart+'@'+addressEnd);
   });
+
+  window.onscroll = function() {scrollFunction()};
+
+  function scrollFunction() {
+    document.body.scrollTop > 20 || document.documentElement.scrollTop > 20 ? $("#myBtn").show() : $("#myBtn").hide();
+  }
+
+  function topFunction() {
+    $("html, body").animate({ scrollTop: 0 }, "slow");
+  }
